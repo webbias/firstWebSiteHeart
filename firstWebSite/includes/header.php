d96ef343d4450aa84c9d89d4dd7c8e6e308a8d4d
Mark-Anthony Baker <124019494+webbias@users.noreply.github.com>
2025-07-23 22:26:25 -0500

    commit_1

---
+++ firstWebSite/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- meta data, doesnt appear on page -->
+<head>
+  <meta charset="UTF-8">
+  <title>firstWebPage</title>
+  <link rel="stylesheet" href="/firstWebPage/css/style.css" media="screen">
+</head>
+<!-- Body, appears on page -->
+ <!-- form ui/ beautification -->
+  
+<body>
+      
+      <div class= "header">
+      <nav class= "navbar">
+        <div class=logo >
+          <a style="background-color: transparent; padding: unset;" href="Mark-Anthony_Baker_ContactUs.php">
+          <img src="https://img.freepik.com/premium-vector/letter-mb-bm-creative-modern-elegant-vector-logo-design-luxury-business_799995-1290.jpg" alt="logo">
+          </a>
+          
+        </div>
+        <ul>
+          <li><a href="School_Registration.php">School Registration</a></li>
+          <li><a href="Mark-Anthony_Baker_ContactUs.php">Contact Us</a></li>
+          <li><a href="School_Registration.php">School Registration</a></li>
+          <li><a href="School_Registration.php">School Registration</a></li>
+        </ul>
+      </nav>
+      </div>
